7313887ab2ce8c19ae56aa721a2cee7da02c761a
Roman Matsypula <ukrmatsypula@gmail.com>
2021-12-04 00:37:37 +0200

    create interceptor for response

--- src/plugins/axios/interceptors.js
+++ src/plugins/axios/interceptors.js
@@ -8,6 +8,11 @@ function setParams(config) {
   return config;
 }
 
+function returnData(res) {
+  return res.data;
+}
+
 export default function (axios) {
   axios.interceptors.request.use(setParams);
+  axios.interceptors.response.use(returnData);
 }
