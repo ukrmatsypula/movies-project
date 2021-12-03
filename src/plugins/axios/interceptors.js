c3f9755d4b04bc3badf5473cec3e0f2de23aa4d4
Roman Matsypula <ukrmatsypula@gmail.com>
2021-12-03 21:06:46 +0200

    created axios intersepter and configure it

---
+++ src/plugins/axios/interceptors.js
@@ -0,0 +1,13 @@
+function setParams(config) {
+  const params = config.params || {};
+  config.params = Object.assign(params, {
+    apiKey: process.env.VUE_APP_API_KEY,
+    plot: "full",
+  });
+
+  return config;
+}
+
+export default function (axios) {
+  axios.interceptors.request.use(setParams);
+}
