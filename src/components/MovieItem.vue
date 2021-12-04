cb95066911abbd5b38c2aaaca71bcbaffd7c498c
Roman Matsypula <ukrmatsypula@gmail.com>
2021-12-04 16:34:40 +0200

    render posters

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>Movie: {{ movie.Title }}</div>
+  <div class="movie-item mb-3">
+    <div class="movie-item__poster" :style="posterBg"></div>
+  </div>
 </template>
 
 <script>
@@ -11,5 +13,40 @@ export default {
       required: true,
     },
   },
+  computed: {
+    posterBg() {
+      return {
+        "background-image": `url(${this.movie.Poster})`,
+      };
+    },
+  },
 };
 </script>
+
+<style scoped>
+.movie-item {
+  position: relative;
+  cursor: pointer;
+  border-radius: 5px;
+  overflow: hidden;
+  transition: all 0.2s ease;
+  height: 400px;
+}
+
+.movie-item:hover {
+  box-shadow: 0px 5px 30px rgba(0, 0, 0, 0.7);
+  transform: scale(1.02);
+}
+
+.movie-item__poster {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: 1;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+}
+</style>
