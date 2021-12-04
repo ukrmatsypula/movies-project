8c85546ff906410166c04a8380e2ee0c4ad714f0
Roman Matsypula <ukrmatsypula@gmail.com>
2021-12-05 00:31:04 +0200

    configurate pagination and set align center

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <PosterBg :poster="posterBg" />
     <MoviesList :list="moviesList" @changePoster="onChangePoster" />
+    <MoviesPagination />
   </div>
 </template>
 
@@ -9,10 +10,11 @@
 import { mapGetters } from "vuex";
 import MoviesList from "@/components/MoviesList";
 import PosterBg from "@/components/PosterBg";
+import MoviesPagination from "@/components/MoviesPagination";
 
 export default {
   name: "App",
-  components: { MoviesList, PosterBg },
+  components: { MoviesList, PosterBg, MoviesPagination },
   computed: {
     ...mapGetters("movies", ["moviesList"]),
   },
---
+++ src/components/MoviesPagination.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="movies-pagination">
+    <BPagination
+      v-model="currentPage"
+      :per-page="perPage"
+      :total-rows="total"
+      prev-text="Prev"
+      next-text="Next"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MoviesPagination",
+  data: () => ({
+    currentPage: 1,
+    total: 100,
+    perPage: 12,
+  }),
+};
+</script>
+
+<style scoped>
+.movies-pagination {
+  display: flex;
+  justify-content: center;
+  margin-top: 30px;
+}
+
+.movies-pagination >>> .pagination .page-item .page-link {
+  color: #fff;
+  background-color: transparent;
+  font-size: 12px;
+}
+
+.movies-pagination >>> .pagination .page-item.active .page-link {
+  border-color: #fff;
+  background-color: #fff;
+  color: #000;
+}
+
+.movies-pagination >>> .pagination .page-item.disabled .page-link {
+  color: gray;
+}
+</style>
