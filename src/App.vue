56ca708555e85cd1ad5bedfb02534ec1da07b90d
Roman Matsypula <ukrmatsypula@gmail.com>
2021-12-04 15:31:55 +0200

    remove mounter in App components

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,12 @@
 <template>
-  <div id="app">
-    <b-button>Button</b-button>
-    <b-button variant="danger">Button</b-button>
-    <b-button variant="success">Button</b-button>
-    <b-button variant="outline-primary">Button</b-button>
-  </div>
+  <div id="app"></div>
 </template>
 
 <script>
-import { mapActions } from "vuex";
 export default {
   name: "App",
   components: {},
-  mounted() {
-    this.fetchMovies();
-  },
-  methods: {
-    ...mapActions("movies", ["fetchMovies"]),
-  },
+  methods: {},
 };
 </script>
 
