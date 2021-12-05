b9af98c42cd91fead1eb5dd89d509ed3da41d808
Roman Matsypula <ukrmatsypula@gmail.com>
2021-12-05 18:17:02 +0200

    added loader

--- src/store/modules/movies.js
+++ src/store/modules/movies.js
@@ -39,8 +39,9 @@ const moviesStore = {
     },
   },
   actions: {
-    async fetchMovies({ getters, commit }) {
+    async fetchMovies({ getters, commit, dispatch }) {
       try {
+        dispatch("toggleLoader", true, { root: true });
         const { currentPage, moviesPerPage, slicedIDs } = getters;
         const from = currentPage * moviesPerPage - moviesPerPage;
         const to = currentPage * moviesPerPage;
@@ -53,6 +54,8 @@ const moviesStore = {
         commit(MOVIES, movies);
       } catch (err) {
         console.log(err);
+      } finally {
+        dispatch("toggleLoader", false, { root: true });
       }
     },
     changeCurrentPage({ commit, dispatch }, page) {
